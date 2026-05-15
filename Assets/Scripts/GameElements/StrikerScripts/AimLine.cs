2c36ba9cc0ed0423bc5b67be431eee4bb370b645
VisionXR <visionxr.opc@gmail.com>
2026-05-15 20:34:17 +0530

    changes

--- Assets/Scripts/GameElements/StrikerScripts/AimLine.cs
+++ Assets/Scripts/GameElements/StrikerScripts/AimLine.cs
@@ -12,17 +12,16 @@ public class AimLine : MonoBehaviour
     public Renderer lineRenderer;
     public GameObject line;
     public GameObject quadCircle;
-//    public GameObject NormalLine;
     
 
     [Header("Properties")]
     public LayerMask colliderMask;
     public float CutOffLength = 1f;
+    public float arrowCutOffLength = 0.15f;
     public float LineThickness = 0.08f;
     public int NoofArrowsPerUnit = 5; // 5 arrows per 0.1m
-    public float distanceFactor = 0.15f; // Adjust this based on your line's original length to get the correct scaling
-    public float checkFactor = 1.5f;
-
+    public float distanceFactor = 0.075f; // Adjust this based on your line's original length to get the correct scaling
+    
 
     public float lineOffset = 0.01f;
     public float strikerOffset = 0.01f;
@@ -45,48 +44,41 @@ public class AimLine : MonoBehaviour
     private void FixedUpdate()
     {
         // 1. Initial Overlap Check - Hide line if overlapping something
-        if (Physics.CheckSphere(checkTransform.position, boardData.StrikerRadius*checkFactor, colliderMask))
-        {
-               line.SetActive(false);
-               quadCircle.SetActive(false);
-             //  NormalLine.SetActive(false);
-              
-            return;
-        }
-
-        line.SetActive(true);
-        quadCircle.SetActive(true);
-     //   NormalLine.SetActive(true);
-
-        Vector3 rayOrigin = transform.position;
-        Vector3 rayDirection = transform.forward;
+   
         float currentDistance = CutOffLength;
+        float hitDistance;
 
         // 2. Perform SphereCast to find the hit point
-        if (Physics.SphereCast(rayOrigin, boardData.StrikerRadius, rayDirection, out hit, CutOffLength, colliderMask))
+        if (Physics.SphereCast(checkTransform.position, boardData.StrikerRadius, checkTransform.forward, out hit, CutOffLength, colliderMask))
         {
+            
+
             currentDistance = hit.distance;
-            quadCircle.transform.position = hit.point + hit.normal * (boardData.StrikerRadius-strikerOffset); // Slightly above the surface to avoid z-fighting
-
-
-            Transform coin = hit.collider.transform;
-
-     //       NormalLine.transform.position = hit.collider.transform.position; // Position at the hit point
            
-
-     //       NormalLine.transform.rotation = Quaternion.LookRotation(-Vector3.up,-hit.normal);
-           
+            hitDistance = currentDistance / distanceFactor;
+            Debug.Log("hit Distance " + hitDistance);
+            if (hitDistance < 1)
+            {
+                line.SetActive(false);
+                quadCircle.SetActive(false);
+            }
+            else
+            {
+                line.SetActive(true);
+                quadCircle.SetActive(true);
+                quadCircle.transform.position = hit.point + hit.normal * (boardData.StrikerRadius - strikerOffset); // Slightly above the surface to avoid z-fighting
+                                                                                                                    // 3. Scale the Line (Assuming Y-axis is the "length" of your line object)
+                currentDistance = Vector3.Distance(line.transform.position, quadCircle.transform.position);
+                Debug.Log("current Distance " + currentDistance);
+                currentDistance = currentDistance / 0.75f;// Adjust the scale to match the distance
+                line.transform.localScale = new Vector3(LineThickness, currentDistance - lineOffset, LineThickness);
+            }
         }
-     
-
-        currentDistance = currentDistance / distanceFactor;
-
-
-        // 3. Scale the Line (Assuming Y-axis is the "length" of your line object)
-        // Adjust the scale to match the distance
-        line.transform.localScale = new Vector3(LineThickness, currentDistance-lineOffset, LineThickness);
-
-       
+        else
+        {
+            line.SetActive(false);
+            quadCircle.SetActive(false);
+        }
 
     }
 }
