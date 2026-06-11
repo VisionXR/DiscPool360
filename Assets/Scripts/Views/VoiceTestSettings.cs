89c63d81754ae0f4fe7619510c14c4f84b375236
VisionXR <visionxr.opc@gmail.com>
2026-06-11 21:09:50 +0530

    changes

--- Assets/Scripts/Views/VoiceTestSettings.cs
+++ Assets/Scripts/Views/VoiceTestSettings.cs
@@ -1,6 +1,7 @@
 using com.VisionXR.GameElements;
 using com.VisionXR.ModelClasses;
 using Photon.Voice.Unity;
+using POpusCodec.Enums;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,7 +20,46 @@ public class VoiceTestSettings : MonoBehaviour
     public TMP_Text targetLevelText;
     public TMP_Text streamDelayText;
 
+    public TMP_Dropdown samplingDD;
+    public TMP_Dropdown micTypeDD;
 
+
+
+    private void OnEnable()
+    {
+        Player mp = playerData.GetMainPlayer();
+
+        if (mp == null)
+        {
+            Debug.LogError("Main player not found!");
+            return;
+        }
+
+        Recorder mainRecorder = mp.GetComponent<PlayerVoiceControl>().recorder;
+        WebRtcAudioDsp dsp = mp.GetComponent<PlayerVoiceControl>().dsp;
+
+
+        gainText.text = "Gain: " + dsp.AgcCompressionGain.ToString();
+        gainSlider.value = dsp.AgcCompressionGain;
+
+        targetLevelText.text = "Target Level: " + dsp.AgcTargetLevel.ToString();
+        targetLevelSlider.value = dsp.AgcTargetLevel;
+
+        streamDelayText.text = "Delay: " + dsp.ReverseStreamDelayMs.ToString() + "ms";
+        streamDelaySlider.value = dsp.ReverseStreamDelayMs;
+
+       
+        if(mainRecorder.MicrophoneType == Recorder.MicType.Unity)
+        {
+            micTypeDD.value = 0;
+        }
+        else
+        {
+            micTypeDD.value = 1;
+        }
+
+        samplingDD.value = (int)mainRecorder.SamplingRate;
+    }
 
 
     public void GainChanged()
@@ -37,7 +77,9 @@ public class VoiceTestSettings : MonoBehaviour
 
         WebRtcAudioDsp dsp = mp.GetComponent<PlayerVoiceControl>().dsp;
 
+
         dsp.AgcCompressionGain = (int)gainSlider.value;
+
     }
 
     public void TargetLevelChanged()
@@ -72,5 +114,29 @@ public class VoiceTestSettings : MonoBehaviour
         WebRtcAudioDsp dsp = mp.GetComponent<PlayerVoiceControl>().dsp;
 
         dsp.ReverseStreamDelayMs = (int)streamDelaySlider.value;
+    }
+
+    public void MicTypeChanged()
+    {
+        Player mp = playerData.GetMainPlayer();
+        Recorder mainRecorder = mp.GetComponent<PlayerVoiceControl>().recorder;
+
+        if (micTypeDD.value == 0)
+        {
+            mainRecorder.MicrophoneType = Recorder.MicType.Unity;
+
+        }
+        else
+        {
+            mainRecorder.MicrophoneType = Recorder.MicType.Photon;
+        }
+    }
+
+    public void SamplingRateChanged()
+    {
+        Player mp = playerData.GetMainPlayer();
+        Recorder mainRecorder = mp.GetComponent<PlayerVoiceControl>().recorder;
+
+        mainRecorder.SamplingRate = (SamplingRate)samplingDD.value;
     }
 }
